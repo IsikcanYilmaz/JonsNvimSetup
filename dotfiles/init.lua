930c8f87d6d6ecd48ccf65bbddda8c47732d4ec6
Isikcan Yilmaz <can158@gmail.com>
2023-12-18 23:08:26 +0100

    set color to abstract

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -196,7 +196,7 @@ require('lazy').setup({
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      vim.cmd.colorscheme 'abstract' --'onedark'
     end,
   },
 
@@ -207,7 +207,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = true,
-        theme = 'onedark',
+        theme = 'abstract',
         component_separators = '|',
         section_separators = '',
       },
