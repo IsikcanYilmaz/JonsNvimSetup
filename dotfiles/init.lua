3766f2f723bb9aef3fbd49089cbb06c8ec2a4efe
Isikcan Yilmaz <can158@gmail.com>
2024-01-20 14:51:51 +0100

    nvim: fidget legacy repo due to it being rewritten

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -88,7 +88,14 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      {
+        "j-hui/fidget.nvim",
+        tag = "legacy",
+        event = "LspAttach",
+        opts = {
+          -- options
+        }
+      },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
